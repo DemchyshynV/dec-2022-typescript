415e010f0ea3b0eef65fc293c8f42a0a2ff01d8a
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2023-04-05 21:30:05 +0300

    hw

--- app.ts
+++ app.ts
@@ -1,152 +1,147 @@
-// // // // const func = (str: number): Function => {
-// // // //     console.log('hello');
-// // // //     return () => console.log('aaaa')
-// // // // }
-// // // //
-// // // //
-// // // // func(5)
-// // // //
-// // // // func(1.5)
-// // //
-// // //
-// // // interface IUser<T> {
-// // //     name: string;
-// // //     age: number;
-// // //     status: boolean;
-// // //     data: T;
-// // // }
-// // //
-// // // // const user1: IUser<number[]> = {name: 'Max', status: true, age: 15, data: [1, 2, 3,]}
-// // // // const user2: IUser<string> = {name: 'Max', status: true, age: 15, data: 'sssss'}
-// // // //
-// // // // const user3: Partial<IUser<number>> = {name: 'Karina', hhdhdhd: 111}
-// // //
-// // //
-// // // class User {
-// // //
-// // //     constructor(protected id: number, private name: string, private age: number) {
-// // //         this.id = id;
-// // //         this.name = name;
-// // //         this.age = age;
-// // //     }
-// // //
-// // //     getName() {
-// // //         return this.name
-// // //     }
-// // // }
-// // //
-// // //
-// // // const user = new User(15, 'Max', 15);
-// // //
-// // // console.log(user.getName());
-// // //
-// // //
-// // interface IShapeActions {
-// //     area: () => number;
-// //     perimeter: () => number;
-// // }
-// // interface IGreeting{
-// //     print:()=>void;
-// // }
-// // //
-// // class Rectangle implements IShapeActions, IGreeting {
-// //     constructor(private a: number, private b: number) {
-// //     }
-// //
-// //     print(): void {
-// //         console.log('hello');
-// //     }
-// //
-// //     area(): number {
-// //         return this.a * this.b
-// //     }
-// //
-// //     perimeter(): number {
-// //         return (this.a + this.b) * 2
-// //     }
-// //
-// // }
-// //
-// // const rectangle = new Rectangle(2,3);
-// // rectangle.print()
-// // //
-// // // class Triangle implements IShapeActions {
-// // //     constructor(private a: number, private b: number, private c: number) {
-// // //     }
-// // //
-// // //     area(): number {
-// // //         return 88
-// // //     }
-// // //
-// // //     perimeter(): number {
-// // //         return this.c + this.c + this.b
-// // //     }
-// // //
-// // // }
-// // // Triangle.area()
-// // //
-// // // const shapes: IShapeActions[] = [
-// // //     new Triangle(1, 2, 3),
-// // //     new Rectangle(3, 6),
-// // //     new Triangle(2, 3, 5)
-// // // ]
-// // //
-// // // for (let shape of shapes) {
-// // //     console.log(shape.area());
-// // //     console.log(shape.perimeter());
-// // // }
-// // //
-// // // console.log('kjsdhfkshdfj');
-// //
-// //
-// // console.log('hello');
-// //
-//
-//
-// const asd = () => {
-//     return 18
-// }
-//
-// // type MyNumber = number|string;
-// //
-// // const asdffff:MyNumber = tru
-//
-//
-// // type MyFuncType = ReturnType<typeof asd>
-// //
-// // const g:MyFuncType = 'dd'
-//
-//
-
-
-import {userService} from './services/user.service';
-
-// userService.getAll().then(value => value.data).then(users => {
-//     for (let user of users) {
-//         console.log(user.email);
-//     }
-// })
-
-// const start = async ()=> {
-//     const res = await userService.getAll();
-//     const users = res.data;
-//     for (let user of users) {
-//
-//     }
-
-// }
-// const asdf = (id: number): void => {
-//     console.log(id);
-// }
-//
-// asdf('ssss')
-
-
-enum DaysOfWeek{
-    MON=10,
-    TUE,
-    WED
-
+// 1) створити інтерфейс на основі цього объекта:
+//     Зверніть увагу там де масиви... в них може бути багато однотипних обїектів
+interface ICore {
+    flight: number
+    core: {
+        reuse_count: number
+        status: string
+    }
 }
 
-console.log(DaysOfWeek.MON);
+interface IPayload {
+    payload_type: string
+    payload_mass_kg: number
+    payload_mass_lbs: number
+}
+
+interface IMission {
+    mission_name: string
+    launch_date_local: string
+    launch_site: {
+        site_name_long: string
+    },
+    links: {
+        article_link: string
+        video_link: string
+    },
+    rocket: {
+        rocket_name: string
+        first_stage: {
+            cores: ICore[]
+        },
+        second_stage: {
+            payloads: IPayload[]
+        }
+    }
+}
+
+
+const myMisson: IMission = {
+    mission_name: 'skjdfhksjdhf',
+    links: {
+        article_link: 'sdfsdf',
+        video_link: 'sdfsfd'
+    },
+    rocket: {
+        rocket_name: 'sdfsdf',
+        first_stage: {
+            cores: [
+                {
+                    core: {reuse_count: 1, status: 'dddd'},
+                    flight: 2
+                }
+            ]
+        },
+        second_stage: {
+            payloads: [
+                {
+                    payload_type: 'ssss',
+                    payload_mass_kg: 111,
+                    payload_mass_lbs: 333
+                },
+                {
+                    payload_type: 'ssss',
+                    payload_mass_kg: 111,
+                    payload_mass_lbs: 333
+                },
+                {
+                    payload_type: 'ssss',
+                    payload_mass_kg: 111,
+                    payload_mass_lbs: 333
+                }
+            ]
+        }
+
+    }
+}
+myMisson.rocket.first_stage.cores.push({fl})
+// ---------------------------------------------------------------------------—————————
+// 2) протипізувати функції:
+interface IUser {
+    name: string
+    age: number
+    gender: string
+}
+
+const user = {
+    name: "Max",
+    age: 18,
+    gender: 'male',
+    sfsdf:'sss'
+}
+
+
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+function showSum(a: number, b: number): void {
+    console.log(a + b);
+}
+
+function incAge(someUser: IUser, inc: number): any {
+    someUser.age += inc
+    return someUser
+}
+
+console.log(sum(1, 2));
+showSum(2, 3)
+incAge(user, 2)
+
+
+interface IShapeActions {
+    area: () => number;
+    perimeter: () => number;
+}
+
+class Rectangle implements IShapeActions {
+    constructor(private a: number, private b: number) {
+    }
+
+    area(): number {
+        return this.a * this.b;
+    }
+
+    perimeter(): number {
+        return this.a + this.b;
+    }
+}
+
+class Triangle implements IShapeActions {
+    constructor(private a: number, private b: number, private c: number, private h: number) {
+    }
+
+    area(): number {
+        return (this.a * this.h) / 2;
+    }
+
+    perimeter(): number {
+        return this.a + this.b + this.c;
+    }
+}
+
+const shapes: IShapeActions[] = [new Rectangle(2, 2), new Triangle(4, 3, 4, 5)];
+for (let shape of shapes) {
+    console.log(shape.perimeter());
+    console.log(shape.area());
+}
